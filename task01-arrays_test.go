2b7cf864eefcb0760d923e3a684df50ef174069e
Nurlan <ikhsan.nurlan@gmail.com>
2022-05-15 14:40:07 +0600

    add: test files

--- task01-arrays_test.go
+++ task01-arrays_test.go
@@ -1,11 +1,13 @@
 package homework
 
-//func TestAverage(t *testing.T) {
-//	val := [15]float32{1, 2, 3, 4, 5, 6}
-//
-//	want := float32(3.5)
-//
-//	if got := average(val); got != want {
-//		t.Errorf("Average(), got: %v, wanted: %v", got, want)
-//	}
-//}
+import "testing"
+
+func TestAverage(t *testing.T) {
+	val := [15]float32{1, 2, 3, 4, 5, 6}
+
+	want := float32(3.5)
+
+	if got := average(val); got != want {
+		t.Errorf("Average(), got: %v, wanted: %v", got, want)
+	}
+}
--- task02-slice_test.go
+++ task02-slice_test.go
@@ -1,17 +1,19 @@
 package homework
 
-//func TestSlice(t *testing.T) {
-//	val := []int64{1, 2, 5, 15}
-//	want := []int64{15, 5, 2, 1}
-//
-//	got := reverse(val)
-//
-//	for i := 0; i < len(got); i++ {
-//		for j := 0; j < len(want); j++ {
-//			if got[i] == want[j] {
-//				break
-//			}
-//		}
-//	}
-//
-//}
+import "testing"
+
+func TestSlice(t *testing.T) {
+	val := []int64{1, 2, 5, 15}
+	want := []int64{15, 5, 2, 1}
+
+	got := reverse(val)
+
+	for i := 0; i < len(got); i++ {
+		for j := 0; j < len(want); j++ {
+			if got[i] == want[j] {
+				break
+			}
+		}
+	}
+
+}
